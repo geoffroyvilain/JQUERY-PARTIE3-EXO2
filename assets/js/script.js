5924e16ac0236bc4f51406646378fc13bac174de
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-02-05 15:11:14 +0100

    exo corrigé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 $(function() {
-  var count = 0;
+  var count = 0; //compteur à 0 ici, en dehors de la fonction, ne se remet aps à 0
   $('#clickBetter').click(function() {
-    count++;
-    $('#number').text(count);
+    count++; //RAJOUTE 1 AU COMPTEUR
+    $('#number').val(count);
   });
   $('#clickLower').click(function() {
-    count--;
-    $('#number').text(count);
+    count--; // DIMINUE DE 1 AU COMPTEUR
+    $('#number').val(count);
   });
 });
